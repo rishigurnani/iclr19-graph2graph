ca554818a95ca5f00142c1f40365bcffa64abbe0
Kenny Scharm <kscharm3@gatech.edu>
2020-04-05 18:07:31 -0400

    Testing new diversity function

--- scripts/poly_diversity.py
+++ scripts/poly_diversity.py
@@ -15,27 +15,38 @@ def similarity(a, b):
     fp2 = AllChem.GetMorganFingerprintAsBitVect(bmol, 2, nBits=2048, useChirality=False)
     return DataStructs.TanimotoSimilarity(fp1, fp2)
 
-def diversity(pairs, num_decode):
+def diversity(pairs):
     diversity_values = []
-    num_test = set()
-    for i in xrange(0, len(pairs), num_decode):
-        data = pairs[i:i + num_decode]
+    sources = set()
+    decoded = {}
+    # Build decoded dictionary that maps source polymers to the list of translated polymers
+    for i in xrange(pairs):
+        source = pairs[i][0]
+        translated = pairs[i][1]
+        sources.add(source)
+        if source in decoded:
+            decoded[source].append(translated)
+        else:
+            decoded[curr_source] = [translated]
+    print(decoded)
+    # Iterate over source molecules in dictionary and determine individual diversity scores
+    for source in decoded:
         div = 0.0
         total = 0
-        for i in xrange(len(data)):
-            num_test.add(data[i][0])
-            for j in xrange(i + 1, len(data)):
-                div += 1 - similarity(data[i][1], data[j][1])
+        test_list = decoded[source]
+        if len(test_list) > 1:
+            for test in test_list:
+                div += 1 - similarity(source, test)
                 total += 1
-        div /= total
+            div /= total
         diversity_values.append(div)
-    num_test = list(num_test)
-    print 'Number of test polymers: ' + str(len(num_test))
+    sources = list(sources)
+    print 'Number of test polymers: ' + str(len(sources))
     return np.mean(diversity_values)
 
 if __name__ == "__main__":
     pairs = [('CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1', 'C[C@@H]1CCc2c(sc(NC(=O)c3ccco3)c2C(N)=O)C1C[C@@H]1CCc2c(sc(NC(=O)c3ccco3)c2C(N)=O)C1C[C@@H]1CCc2c(sc(NC(=O)c3ccco3)c2C(N)=O)C1'),
     ('CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1', 'C[C@@H]1CCN(C(=O)CCCc2ccccc2)C[C@@H]1OC[C@@H]1CCN(C(=O)CCCc2ccccc2)C[C@@H]1OC[C@@H]1CCN(C(=O)CCCc2ccccc2)C[C@@H]1O'),
-    ('CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1', 'O=C(NCc1cc(F)cc(F)c1)c1cccnc1O=C(NCc1cc(F)cc(F)c1)c1cccnc1O=C(NCc1cc(F)cc(F)c1)c1cccnc1')]
-    num_decode = 3
-    print diversity(pairs ,num_decode)
+    ('CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1CC(C)(C)[C@H]1CCc2c(sc(NC(=O)COc3ccc(Cl)cc3)c2C(N)=O)C1', 'O=C(NCc1cc(F)cc(F)c1)c1cccnc1O=C(NCc1cc(F)cc(F)c1)c1cccnc1O=C(NCc1cc(F)cc(F)c1)c1cccnc1'),
+    ('COc1cc(Cl)ccc1C(=O)OC[C@H]1CCCCO1', 'C[C@H](Oc1cc(Cl)ccc1Cl)C(=O)NC[C@@H]1CCCO1')]
+    print diversity(pairs)
