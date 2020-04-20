b8e3a8881fa5bd7886705ea983c9aab164128156
Kenny Scharm <kscharm3@gatech.edu>
2020-04-20 13:12:26 -0400

    Rename files and update readme

--- README.md
+++ README.md
@@ -9,18 +9,18 @@ This is the official implementation of junction tree encoder-decoder model in ht
 * Numpy
 * scikit-learn
 
-The code has been tested under python 2.7 with pytorch 0.4.1. 
+The code has been tested under python 2.7 with pytorch 0.4.1.
 
 ## Quick Start
 The tutorial of training and testing our variational junction tree encoder-decoder is in [diff_vae/README.md](./diff_vae).
 
 A quick summary of different folders:
-* `data/` contains the training, validation and test set of logP, QED and DRD2 tasks described in the paper.
-* `fast_jtnn/` contains the implementation of junction tree encoder-decoder.
+* `data_prep/` contains cosine, Jaccard, and Tanimoto similarity preprocessing scripts.
 * `diff_vae/` includes the training and decoding script of variational junction tree encoder-decoder ([README](./diff_vae)).
 * `diff_vae_gan/` includes the training and decoding script of adversarial training module ([README](./diff_vae_gan)).
+* `fast_jtnn/` contains the implementation of junction tree encoder-decoder.
 * `props/` is the property evaluation module, including penalized logP, QED and DRD2 property calculation.
-* `scripts/` provides evaluation and data preprocessing scripts.
+* `scripts/` provides model evaluation, visualization and preprocessing scripts.
 
 ## Contact
 Wengong Jin (wengong@csail.mit.edu)
---
+++ data_prep/calc_features.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import json
+import sys
+
+args= sys.argv
+path_to_data = args[1]
+
+
+fp_all = pd.read_csv(path_to_data)
+ids = fp_all['id']
+
+def get_fp_jaccard(smiles): # smiles already fixed
+    num_bits = 2048
+    if smiles is None:
+        return np.zeros(num_bits)
+    amol = Chem.MolFromSmiles(smiles)
+    if amol is None:
+        return np.zeros(num_bits)
+    fp = AllChem.GetMorganFingerprintAsBitVect(amol, 2, nBits=num_bits, useChirality=False)
+    fp = np.array(fp).astype(int)
+    return fp
+
+
+def get_vec_cosine(p_id):
+    '''
+    Get feature vector from polymer_id
+    '''
+    row = fp_all[fp_all['id'] == p_id]
+    vec = row.drop(['Unnamed: 0', 'id', 'smiles', 'dft_bandgap', 'norm_dft_bandgap', 'mean_dft_bandgap', 'std_dft_bandgap'], axis=1).to_numpy()
+    return vec[0].astype(np.double)
+
+fp_info = {}
+vec_info = {}
+
+for id in ids:
+    bandgap = fp_all.loc[fp_all['id'] == id, 'dft_bandgap'].item()
+    smiles = fp_all.loc[fp_all['id'] == id, 'smiles'].item()
+    smiles = smiles.replace('[*]', '') 
+    print('current id is {}'.format(id))
+    
+    print('------get_fp_jaccard--------')
+    fp = get_fp_jaccard(smiles)
+    print(fp, np.count_nonzero(fp), fp.shape)
+    fp_info[id] = [fp.tolist(), bandgap, smiles*3]
+
+    print('------get_vec_cosine--------')
+    vec = get_vec_cosine(id)
+    # print(vec)
+    print(vec.shape)
+    vec_info[id] = [vec.tolist(), bandgap, smiles*3]
+
+fp_info = [{'p_id': k, 'fp': v[0], 'dft_bandgap': v[1], 'smiles': v[2]} for k, v in fp_info.items()]
+with open('fp_info.json', 'w') as f:
+    json.dump(fp_info, f)
+
+
+vec_info = [{'p_id': k, 'fp': v[0], 'dft_bandgap': v[1], 'smiles': v[2]} for k, v in vec_info.items()]
+with open('vec_info.json', 'w') as f:
+    json.dump(vec_info, f)
+
